a9a92eeab29823825a0c694f2c621ea510df7f3d
Mayur <matthewyou47@gmail.com>
2023-01-24 19:10:58 +0530

    Upgrade 1.0

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-
 app.use(express.static(__dirname));
-app.set('view engine', 'html');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname+ '/frontend/index.html');
