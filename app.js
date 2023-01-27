4a28126bf8230ee4b513b789c3b89ae9c81ca50b
Mayur <matthewyou47@gmail.com>
2023-01-27 21:12:59 +0530

    Upgrade 4.0

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+ '/frontend/index.html');
-    })
-    
+    res.sendFile(__dirname + '/frontend/index.html');
+})
+
 app.get('/sign-up', (req, res) => {
-    res.sendFile(__dirname+ '/frontend/sign-up.html');
-    })
+    res.sendFile(__dirname + '/frontend/sign-up.html');
+})
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname+ '/frontend/login.html');
-    })
-    
+    res.sendFile(__dirname + '/frontend/login.html');
+})
+
 app.get('/loby.html', (req, res) => {
-    res.sendFile(__dirname+ '/frontend/loby.html');
-    res.json({username});
-    })
+    res.sendFile(__dirname + '/frontend/loby.html');
+    res.json({
+        username
+    });
+})
 
 app.listen(3000, () => console.log('running at 3000...'));
