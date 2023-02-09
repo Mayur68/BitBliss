4c38cd582b6fa365eb2134e55a0ab6e22365a774
Mayur <matthewyou47@gmail.com>
2023-02-09 15:12:38 +0530

    Upgrade 4.0

--- app.js
+++ app.js
@@ -6,19 +6,40 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/frontend/index.html');
+  res.sendFile(__dirname + '/frontend/index/index.html');
 })
 
 app.get('/sign-up', (req, res) => {
-    res.sendFile(__dirname + '/frontend/sign-up.html');
+  res.sendFile(__dirname + '/frontend/index/register/sign-up.html');
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/frontend/login.html');
+  res.sendFile(__dirname + '/frontend/index/login/login.html');
 })
 
-app.get('/loby.html', (req, res) => {
-    res.sendFile(__dirname + '/frontend/loby.html');
+app.get('/user', (req, res) => {
+  res.sendFile(__dirname + '/frontend/index/user/user.html');
 })
+
+app.post("/login", (req, res) => {
+  const {
+    username,
+    password
+  } = req.body;
+
+  if (username === "admin" && password === "password") {
+    res.json({
+      status: "success",
+      message: "Login successful!",
+    });
+  } else {
+    res.status(401).json({
+      status: "error",
+      message: "Incorrect username or password.",
+    });
+  }
+});
+
+
 
 app.listen(3000, () => console.log('running at 3000...'));
