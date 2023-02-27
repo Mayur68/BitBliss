b557dc003f03df23fe79a00ee6b5a1e7abc518fc
Mayur <89019990+Mayur68@users.noreply.github.com>
2023-02-27 19:50:42 +0530

    Update account.js

--- backend/account.js
+++
@@ -1,9 +0,0 @@
-
-
-const uri = "mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority";
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
-  if (err) throw err;
-  const db = client.db("test");
-  console.log("Connected to MongoDB Atlas");
-  client.close();
-});
