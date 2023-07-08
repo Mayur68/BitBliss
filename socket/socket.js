79afceb84c3ce0baad63eeff961f8aad5b412b74
Urvijeth <138894757+Urvijeth@users.noreply.github.com>
2023-07-08 11:11:59 +0530

    bugfix3

--- socket/socket.js
+++ socket/socket.js
@@ -4,7 +4,7 @@ function setupSocket(server) {
   const io = socketIO(server);
 
   io.on('connection', (socket) => {
-    console.log('A user connected....');
+    console.log('A user connected...');
 
     socket.on('message', (data) => {
       console.log('Received message:', data);
@@ -14,7 +14,7 @@ function setupSocket(server) {
     });
 
     socket.on('disconnect', () => {
-      console.log('A user disconnected');
+      console.log('A user disconnected...');
     });
   });
 }
