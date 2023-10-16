5c0d8666bbd6d1cc175e127e327e31361176e007
Mayur <89019990+Mayur68@users.noreply.github.com>
2023-10-16 18:33:23 +0530

    update

--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ let dbConnection;
 
 module.exports = {
   connection: (cb) => {
-    MongoClient.connect("mongodb://127.0.0.1:27017/webapp")
+    MongoClient.connect("mongodb+srv://mayur68:" + encodeURIComponent("IF8QYZLtEzm7kR6") + "@cluster0.bpkmyqk.mongodb.net/?retryWrites=true&w=majority")
       .then((client) => {
         dbConnection = client.db();
         return cb();
