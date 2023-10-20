bba91d73a430a74516f500b71c1de9204f3a124f
Mayur <89019990+Mayur68@users.noreply.github.com>
2023-10-20 21:57:36 +0530

    update creating repository and storing uploads

--- routes/repository.js
+++ routes/repository.js
@@ -1,35 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const { db, Account, File } = require("../database/database");
+const multer = require("multer");
+const path = require("path");
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const uploadDir = path.join(__dirname, "../uploads");
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
 
-router.post("/createRepository", async (req, res) => {
+const upload = multer({ storage: storage });
+
+router.post("/createRepository", upload.single("file"), async (req, res) => {
     try {
-        if (!db) {
-            console.error("Database connection not established");
-            return res.status(500).json({ error: "Internal Server Error" });
-        }
-
-        // First, find the user with the provided username
-        const user = await Account.findOne({ username: req.body.accountId });
-
-        if (!user) {
-            return res.status(404).json({ error: "User not found" });
-        }
-
         const newRepository = new File({
             name: req.body.name,
-            owner: user._id, // Use user._id to get the user's ObjectId
+            owner: req.body.accountId,
             createdAt: new Date(),
-            filePath: req.body.filePath,
+            filePath: req.file.path,
         });
 
-        // Save the new repository using Mongoose
         await newRepository.save();
 
         res.status(201).json({ message: "Repository created successfully" });
     } catch (error) {
-        console.error(error); // Log the error for debugging
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
